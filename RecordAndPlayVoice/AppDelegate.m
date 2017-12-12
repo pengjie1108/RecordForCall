6e4e3558d6b454696efabea8e6fd2359af1e450e
PJ <237520818@qq.com>
2017-12-12 10:16:17 +0800

    iOS10使用CXCallObserver作为电话观察者

--- RecordAndPlayVoice/AppDelegate.m
+++ RecordAndPlayVoice/AppDelegate.m
@@ -9,13 +9,12 @@
 #import "AppDelegate.h"
 #import <CoreTelephony/CTCall.h>
 #import <CoreTelephony/CTCallCenter.h>
+#import <CallKit/CallKit.h>
 #import "LVRecordTool.h"
 
-@interface AppDelegate ()
+@interface AppDelegate ()<CXCallObserverDelegate>
 
-@property (strong, nonatomic) CTCallCenter *callCenter;
-
-//@property (nonatomic, strong) AVAudioRecorder *recorder;
+@property (strong, nonatomic) CXCallObserver *callObserver;
 
 @end
 
@@ -23,43 +22,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     
-    CTCallCenter *callCenter = [[CTCallCenter alloc] init];
+    CXCallObserver *callObserver = [[CXCallObserver alloc] init];
+    [callObserver setDelegate:self queue:nil];
+    self.callObserver = callObserver;
     
-    _callCenter = callCenter;
-    
-    self.callCenter.callEventHandler = ^(CTCall* call) {
-        if ([call.callState isEqualToString:CTCallStateDisconnected])
-        {
-            NSLog(@"挂断了电话咯Call has been disconnected");
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [[LVRecordTool sharedRecordTool] resumeRecording];
-
-            });
-        }
-        else if ([call.callState isEqualToString:CTCallStateConnected])
-        {
-            NSLog(@"电话通了Call has just been connected");
-            
-        }
-        else if([call.callState isEqualToString:CTCallStateIncoming])
-        {
-            NSLog(@"来电话了Call is incoming");
-            [[LVRecordTool sharedRecordTool] pauseRecording];
-            
-        }
-        else if ([call.callState isEqualToString:CTCallStateDialing])
-        {
-            NSLog(@"正在播出电话call is dialing");
-        }
-        else
-        {
-            NSLog(@"嘛都没做Nothing is done");
-        }
-    };
-    
-
     return YES;
 }
 
@@ -83,6 +50,22 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call{
+    if (call.hasEnded) {
+        NSLog(@"挂断了电话咯   Call has been disconnected");
+          dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+          [[LVRecordTool sharedRecordTool] resumeRecording];
+          });
+    }else if (call.hasConnected){
+        NSLog(@"电话通了Call has just been connected");
+    }else if(call.onHold){
+        NSLog(@"来电话了Call is incoming");
+        [[LVRecordTool sharedRecordTool] pauseRecording];
+    }else if (call.outgoing){
+         NSLog(@"电话不通");
+    }
 }
 
 @end
